dbb574f151904cc72662b623b3a07851fb34a626
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-06-25 20:21:48 -0700

    created a method to swap the nth node from the end and nth node form the first

--- src/SwapNthNodesLL.java
+++ src/SwapNthNodesLL.java
@@ -7,7 +7,32 @@ public class SwapNthNodesLL {
             this.next = null;
         }
     }
+    public static Node swapNthNodesLL(Node head, int n){
+        int jump1 = n-1;
+        Node start = head;
+        while(jump1>0){
+            jump1 --;
+            start = start.next;
+        }
+        int size = 0;
+        Node current = head;
+        while(current != null){
+            size ++;
+            current = current.next;
+        }
+        int jump2= size - n;
+        Node end = head;
+        while(jump2 > 0){
+            jump2--;
+            end = end.next;
+        }
+        int temp = start.data;
+        start.data = end.data;
+        end.data = temp;
+        return head;
+    }
     public static void main(String[] args) {
+
 
     }
 }
