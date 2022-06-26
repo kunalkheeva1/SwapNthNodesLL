966e04efed1d8aab5d05d661f43425458f7583ab
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-06-25 20:13:37 -0700

    created a node class

--- src/SwapNthNodesLL.java
+++ src/SwapNthNodesLL.java
@@ -1,5 +1,12 @@
 public class SwapNthNodesLL {
-    
+    static class Node {
+        int data;
+        Node next;
+        Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
     public static void main(String[] args) {
 
     }
