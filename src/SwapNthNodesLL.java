fd32e475b18eefc676ddf40fb040197f20c8e7f1
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-06-25 20:24:23 -0700

    added annotations for better understandings

--- src/SwapNthNodesLL.java
+++ src/SwapNthNodesLL.java
@@ -8,24 +8,32 @@ public class SwapNthNodesLL {
         }
     }
     public static Node swapNthNodesLL(Node head, int n){
+
+        //so its number of jumps i need to reach my  nth node from the head node
         int jump1 = n-1;
         Node start = head;
+        //after this loop my start node is at nth node from beginning
         while(jump1>0){
             jump1 --;
             start = start.next;
         }
+
+        //now lets get the size of the list
         int size = 0;
         Node current = head;
         while(current != null){
             size ++;
             current = current.next;
         }
+
+        // size - n is the measurement to get the nth node from the end by traversing from the beginning
         int jump2= size - n;
         Node end = head;
         while(jump2 > 0){
             jump2--;
             end = end.next;
         }
+        //once i get start and end node i just swap them and return head
         int temp = start.data;
         start.data = end.data;
         end.data = temp;
